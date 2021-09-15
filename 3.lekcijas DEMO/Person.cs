fc43b8ed344ba55fc809079c7c502cdc5b088203
Edgars Raginskis <raginskis@gmail.com>
2021-09-15 20:12:43 +0300

    Add project files.

---
+++ 3.lekcijas DEMO/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.lekcijas_DEMO
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        private int _age;
+        public int Age
+        {
+            get 
+            {
+                return _age;
+            }
+            set 
+            {
+                _age = Math.Abs(value);
+            }
+        }
+
+        public double Money { get; set; }
+
+        public bool IsMale { get; set; }
+
+        public char Initial { get; set; }
+
+        public string GetGreeting()
+        {
+            string greeting = "Hello, my name is " + Name + "and I am " + Age + "years old";
+        
+            return greeting;
+
+        }
+
+
+    }
+}
